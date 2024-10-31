26bc8564f33815db7ffc15001ee574348d9589b2
Arhin <113555698+Arhinsingh05@users.noreply.github.com>
2024-10-31 12:11:53 -0300

    primer commnit

---
+++ assets/js/popup.js
@@ -0,0 +1,9 @@
+   // Función para abrir el popup
+   function openPopup() {
+    document.getElementById("popup").style.display = "flex";
+}
+
+// Función para cerrar el popup
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
