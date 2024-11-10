161e565452f76daa4f445083e220387f3eea7e46
Reinaldo Carlos Romano <aldo21406@gmail.com>
2024-11-10 15:40:48 -0300

    agregar mapa

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -7,3 +7,13 @@
 function closePopup() {
     document.getElementById("popup").style.display = "none";
 }
+
+// Función para abrir el popup
+   function openPopupMapa() {
+    document.getElementById("popup-mapa").style.display = "flex";
+}
+
+// Función para cerrar el popup
+function closePopupMapa() {
+    document.getElementById("popup-mapa").style.display = "none";
+}
